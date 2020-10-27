213b4d5173facef52ead02daafcdcff72ce89cd7
Carsten Thiele <software@carsten-thiele.de>
2020-10-27 06:50:59 +0100

    config handling

--- evcc/scripts/run.sh
+++ evcc/scripts/run.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-#cat /data/options.json
-
-#json2yaml /data/options.json > /etc/evcc.yaml
-
-#cat /etc/evcc.yaml
-
-evcc
+if [ ! -f "/config/evcc.yaml" ]; then
+    echo "config not found. Creating default config file in /config/evcc.yaml"
+    cp /evcc/evcc.dist.yaml /config/evcc.yaml
+fi
+echo "starting evcc --config /config/evcc.yaml"
+evcc --config /config/evcc.yaml
