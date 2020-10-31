c82309fe00aa2c1ab3fc3b97bbfd557c1bdf1373
Tscherno <tscherno@gmail.com>
2020-10-31 16:52:31 +0100

    Update run.sh

--- evcc/scripts/run.sh
+++ evcc/scripts/run.sh
@@ -1,12 +1,17 @@
-#!/usr/bin/with-contenv bashio
+#!/usr/bin/env bash
+set -e
+
+CONFIG_PATH=/data/options.json
+
+CONFIG_FILE=$(jq --raw-output '.config_file // empty' $CONFIG_PATH)
 
 echo "evcc addon Startup script run.sh"
 
-echo "Using config $(bashio::config 'config_file')"
+echo "Using config ${CONFIG_FILE}"
 
-if [ ! -f $(bashio::config 'config_file') ]; then
+if [ ! -f ${CONFIG_FILE} ]; then
     echo "config not found. Please see evcc documentation and /config/evcc.dist.yaml for example configuration."
     cp /evcc/evcc.dist.yaml /config/evcc.dist.yaml
 fi
-echo "starting evcc --config $(bashio::config 'config_file')"
-evcc --config $(bashio::config 'config_file')
+echo "starting evcc --config ${CONFIG_FILE}"
+evcc --config ${CONFIG_FILE}
