90d0b42dc1d1c62f7bff874ab7e61f6c67057dca
Carsten Thiele <software@carsten-thiele.de>
2020-10-31 17:54:15 +0100

    pin evcc version 0.30

--- evcc/scripts/run.sh
+++ evcc/scripts/run.sh
@@ -2,7 +2,6 @@
 
 echo "evcc addon Startup script run.sh"
 
-cat /data/options.json
 CONFIG=$(jq .config_file /data/options.json)
 
 echo "Using config ${CONFIG}"
@@ -11,5 +10,6 @@ if [ ! -f ${CONFIG} ]; then
     echo "config not found. Please see evcc documentation and /config/evcc.dist.yaml for example configuration."
     cp /evcc/evcc.dist.yaml /config/evcc.dist.yaml
 fi
+
 echo "starting evcc --config ${CONFIG}"
 evcc --config ${CONFIG}
